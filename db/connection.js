cdb2f10d877fe0abc23d71c92ab76e2d64cc3ef7
Steven Connors <steven.connors53@gmail.com>
2017-12-18 14:40:52 -0500

    more heroku deployment fixes

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 const boolean = require('boolean')
 
-mongoose.connect('mongodb://localhost/telestrations_db', {useMongoClient: true})
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect("mongodb://localhost/telestrations_db", {useMongoClient: true});
+}
 
 module.exports = mongoose
